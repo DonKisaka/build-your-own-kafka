d8fbbe85da88d9ccc3864b69b56e6198c79ba760
DonaldKisaka <davidkisaka4@gmail.com>
2026-03-10 08:51:20 +0300

    Initial commit

---
+++ build-your-own-kafka/src/main/java/com/example/build_your_own_kafka/BuildYourOwnKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.build_your_own_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildYourOwnKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildYourOwnKafkaApplication.class, args);
+	}
+
+}
---
+++ build-your-own-kafka/src/test/java/com/example/build_your_own_kafka/BuildYourOwnKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.build_your_own_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildYourOwnKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
